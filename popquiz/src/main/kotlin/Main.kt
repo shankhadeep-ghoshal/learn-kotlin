1f734f2e1a458006ecc7ccb2aff324a1f9db31f8
Shankhadeep-Ghoshal <shankhadeepghoshal1996@gmail.com>
2022-09-01 09:02:38 +0300

    Implemented nullability example popquiz

--- popquiz/src/main/kotlin/Main.kt
+++ popquiz/src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val s1: String? = null
+    val s2: String? = "" // from example and not to be changed
+    s1.isEmptyOrNull() eq true
+    s2.isEmptyOrNull() eq true
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val s3 = "   "
+    s3.isEmptyOrNull() eq false
+}
+
+private infix fun <T> T.eq(other: T) {
+    if (this == other) println("OK")
+    else println("Error: $this != $other")
+}
+
+private fun String?.isEmptyOrNull(): Boolean {
+    return this == null || this.isEmpty()
 }
