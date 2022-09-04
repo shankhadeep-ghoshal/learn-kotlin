82102886aaee7a77d077b1cb86fdadf79a4c1c20
Shankhadeep-Ghoshal <shankhadeepghoshal1996@gmail.com>
2022-09-04 11:21:00 +0300

    Applied refactored code as per PR suggestions

--- Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -1,38 +1,13 @@
 package mastermind
 
-import kotlin.math.min
-
-data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
-
-fun evaluateGuess(secret: String, guess: String): Evaluation {
-    val mappedSecret = convertToMap(secret)
-    val mappedGuess = convertToMap(guess)
-
-    var wrongPos = 0
-    var rightPos = 0
-    mappedSecret.filter { (key, _) -> mappedGuess.containsKey(key) }.forEach {(key, value) ->
-        run {
-            val guessEntry = mappedGuess[key]
-            val commonIndices = guessEntry?.intersect(value)
-            rightPos += commonIndices?.size ?: 0
-            value.minus(commonIndices!!)
-            guessEntry.minus(commonIndices)
-            wrongPos += min(value.minus(commonIndices).size, guessEntry.minus(commonIndices).size)
-        }
-    }
-
-    return Evaluation(rightPos,wrongPos)
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int) {
+    private val codeLength = 4
+    fun isComplete(): Boolean = rightPosition == codeLength
 }
 
-fun convertToMap(str: String): MutableMap<Char, MutableSet<Int>> {
-    val map = HashMap<Char, MutableSet<Int>>()
-    for (i in str.indices) {
-        if (map.containsKey(str[i])) {
-            map[str[i]]?.add(i)
-        } else {
-            map[str[i]] = mutableSetOf(i)
-        }
-    }
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+    val rightPosition = secret.zip(guess).count { it.first == it.second }
 
-    return map
+    val commonPosition = ('A'..'F').sumBy { ch -> minOf(secret.count { it == ch }, guess.count { it == ch }) }
+    return Evaluation(rightPosition, commonPosition - rightPosition)
 }
--- Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/playMastermind.kt
+++ Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/playMastermind.kt
@@ -11,47 +11,45 @@ fun main() {
 }
 
 fun playMastermind(
-        differentLetters: Boolean,
-        secret: String = generateSecret(differentLetters)
+    differentLetters: Boolean,
+    secret: String = generateSecret(differentLetters)
 ) {
     var evaluation: Evaluation
 
     do {
         print("Your guess: ")
-        var guess = readLine()!!
+        var guess = readLine() ?: ""
         while (hasErrorsInInput(guess)) {
-            println("Incorrect input: $guess. " +
-                    "It should consist of $CODE_LENGTH characters from $ALPHABET. " +
-                    "Please try again.")
-            guess = readLine()!!
+            println(
+                """Incorrect input: $guess. 
+                    |It should consist of $CODE_LENGTH characters from $ALPHABET.
+                    |Please try again.""".trimMargin()
+            )
+            guess = readLine() ?: ""
         }
         evaluation = evaluateGuess(secret, guess)
         if (evaluation.isComplete()) {
             println("The guess is correct!")
         } else {
-            println("Right positions: ${evaluation.rightPosition}; " +
-                    "wrong positions: ${evaluation.wrongPosition}.")
+            println(
+                "Right positions: ${evaluation.rightPosition}; " +
+                        "wrong positions: ${evaluation.wrongPosition}."
+            )
         }
 
     } while (!evaluation.isComplete())
 }
 
-fun Evaluation.isComplete(): Boolean = rightPosition == CODE_LENGTH
-
 fun hasErrorsInInput(guess: String): Boolean {
     val possibleLetters = ALPHABET.toSet()
-    return guess.length != CODE_LENGTH || guess.any { it !in possibleLetters }
+    return guess.length != CODE_LENGTH || guess.none { it in possibleLetters }
 }
 
 fun generateSecret(differentLetters: Boolean): String {
     val chars = ALPHABET.toMutableList()
     return buildString {
-        for (i in 1..CODE_LENGTH) {
-            val letter = chars[Random.nextInt(chars.size)]
-            append(letter)
-            if (differentLetters) {
-                chars.remove(letter)
-            }
-        }
+        (1..CODE_LENGTH)
+            .filter { !differentLetters }
+            .map { append(chars[Random.nextInt(chars.size)]) }
     }
 }
